ef7b18c315d161fafb3762b430c186fb67cb2151
BenCampbell90 <bencampbell90@outlook.com>
2025-11-26 13:30:16 +1100

    Remove Orca model components and related dependencies from Hero section; update package.json and package-lock.json to reflect these changes.

--- src/components/sections/Hero/index.tsx
+++ src/components/sections/Hero/index.tsx
@@ -1,7 +1,6 @@
 import { Link } from "@tanstack/react-router";
 import { ChevronDown } from "lucide-react";
 import "./heroStyles.css";
-import { OrcaCanvas } from "@/components/sections/Hero/components/orcaModel/orcaCanvas";
 
 /**********************************************************************************************************
  *   COMPONENT START
@@ -12,7 +11,6 @@ export const Hero = () => {
 		<section className="hero">
 			<div className="hero__content">
 				<h1 className="hero__title">Your Small Business Best Friend</h1>
-				<OrcaCanvas />
 				<p className="hero__description">
 					ORCA is an industry-leading management system for Aussie small
 					business. Fast, light, and refreshingly straightforward.
